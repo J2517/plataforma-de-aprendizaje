1731caf9339c00ad99177755f185feb724d11709
JFranco404 <jhon.1702011356@ucaldas.edu.co>
2025-05-21 11:09:31 -0500

    Controlador para obtener las preguntas de un examen por ID

--- PAL/src/main/java/com/example/pal/controller/ExamController.java
+++ PAL/src/main/java/com/example/pal/controller/ExamController.java
@@ -1,10 +1,26 @@
 package com.example.pal.controller;
 
+import com.example.pal.model.Exam;
+import com.example.pal.service.ExamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/exam")
 public class ExamController {
-    // TODO: Endpoints para crear exmanes como docente
+
+    @Autowired
+    ExamService examService;
+
+    @GetMapping("/questions/{examId}")
+    public ResponseEntity<Optional> getExamQuestions(@PathVariable("examId") Long examId) {
+        Optional<Exam> examResult = examService.findByID(examId);
+        return ResponseEntity.status(200).body(examResult);
+    }
 }
